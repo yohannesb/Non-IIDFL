bf2f5c2da525967d2dc111a394c8c33a302b4540
yohannesb <yohannesb@users.noreply.github.com>
2025-03-11 17:47:21 +0300

    Update fashion_mnist.py

--- federated_learning/datasets/fashion_mnist.py
+++ federated_learning/datasets/fashion_mnist.py
@@ -45,7 +45,7 @@ class FashionMNISTDataset(Dataset):
     def load_test_dataset(self):
         self.get_args().get_logger().debug("Loading Fashion MNIST test data")
 
-        normalize = transforms.Normalize(mean=[0.5], std=[0.5])  # Mean and std for Fashion MNIST
+        # normalize = transforms.Normalize(mean=[0.5], std=[0.5])  # Mean and std for Fashion MNIST
         transform = transforms.Compose([
             transforms.ToTensor(),
             normalize
