582eeaf13b884a50d2c07755c4413f33d3809ff9
yohannesb <yohannesb@users.noreply.github.com>
2025-03-12 15:16:45 +0300

    Update client.py

--- client.py
+++ client.py
@@ -235,6 +235,189 @@
 
 
 
+# import torch
+# import torch.optim as optim
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import classification_report
+# from federated_learning.schedulers import MinCapableStepLR
+# import os
+# import numpy
+# import copy
+
+# class Client:
+
+#     def __init__(self, args, client_idx, train_data_loader, test_data_loader):
+#         self.args = args
+#         self.client_idx = client_idx
+
+#         self.device = self.initialize_device()
+#         self.set_net(self.load_default_model())
+
+#         self.loss_function = self.args.get_loss_function()()
+
+#         # # Use Adam optimizer
+#         # self.optimizer = optim.Adam(
+#         #     self.net.parameters(),
+#         #     lr=self.args.get_learning_rate(),
+#         #     betas=(self.args.get_beta1(), self.args.get_beta2()),
+#         #     eps=self.args.get_eps()
+#         # )
+
+#         # Use RMSprop optimizer
+#         # self.optimizer = optim.RMSprop(
+#         #     self.net.parameters(),
+#         #     lr=self.args.get_learning_rate(),
+#         #     alpha=0.99,
+#         #     eps=self.args.get_eps(),
+#         #     weight_decay=0,
+#         #     momentum=self.args.get_momentum()
+#         # )
+#          self.optimizer = optim.SGD(self.net.parameters(),
+#              lr=self.args.get_learning_rate(),
+#              momentum=self.args.get_momentum())
+
+#         # Scheduler remains the same
+#         self.scheduler = MinCapableStepLR(
+#             self.args.get_logger(), self.optimizer,
+#             self.args.get_scheduler_step_size(),
+#             self.args.get_scheduler_gamma(),
+#             self.args.get_min_lr()
+#         )
+
+#         self.train_data_loader = train_data_loader
+#         self.test_data_loader = test_data_loader
+
+#     def initialize_device(self):
+#         if torch.cuda.is_available() and self.args.get_cuda():
+#             return torch.device("cuda:0")
+#         else:
+#             return torch.device("cpu")
+
+#     def set_net(self, net):
+#         self.net = net
+#         self.net.to(self.device)
+
+#     def load_default_model(self):
+#         model_class = self.args.get_net()
+#         default_model_path = os.path.join(self.args.get_default_model_folder_path(), model_class.__name__ + ".model")
+
+#         return self.load_model_from_file(default_model_path)
+
+#     def load_model_from_file(self, model_file_path):
+#         model_class = self.args.get_net()
+#         model = model_class()
+
+#         if os.path.exists(model_file_path):
+#             try:
+#                 model.load_state_dict(torch.load(model_file_path))
+#             except:
+#                 self.args.get_logger().warning("Couldn't load model. Attempting to map CUDA tensors to CPU to solve error.")
+#                 model.load_state_dict(torch.load(model_file_path, map_location=torch.device('cpu')))
+
+#         else:
+#             self.args.get_logger().warning("Could not find model: {}".format(model_file_path))
+
+#         return model
+
+#     def get_client_index(self):
+#         return self.client_idx
+
+#     def get_nn_parameters(self):
+#         return self.net.state_dict()
+
+#     def update_nn_parameters(self, new_params):
+#         self.net.load_state_dict(copy.deepcopy(new_params), strict=True)
+
+#     def train(self, epoch):
+#         self.net.train()
+
+#         if self.args.should_save_model(epoch):
+#             self.save_model(epoch, self.args.get_epoch_save_start_suffix())
+
+#         running_loss = 0.0
+#         for i, (inputs, labels) in enumerate(self.train_data_loader, 0):
+#             inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+#             self.optimizer.zero_grad()
+
+#             outputs = self.net(inputs)
+#             loss = self.loss_function(outputs, labels)
+#             loss.backward()
+#             self.optimizer.step()
+
+#             running_loss += loss.item()
+#             if i % self.args.get_log_interval() == 0:
+#                 self.args.get_logger().info('[%d, %5d] loss: %.3f' % (epoch, i, running_loss / self.args.get_log_interval()))
+#                 running_loss = 0.0
+
+#         self.scheduler.step()
+
+#         if self.args.should_save_model(epoch):
+#             self.save_model(epoch, self.args.get_epoch_save_end_suffix())
+
+#         return running_loss
+
+#     def save_model(self, epoch, suffix):
+#         self.args.get_logger().debug("Saving model to flat file storage. Save #{}", epoch)
+
+#         if not os.path.exists(self.args.get_save_model_folder_path()):
+#             os.mkdir(self.args.get_save_model_folder_path())
+
+#         full_save_path = os.path.join(self.args.get_save_model_folder_path(), f"model_{self.client_idx}_{epoch}_{suffix}.model")
+#         torch.save(self.get_nn_parameters(), full_save_path)
+
+#     def calculate_class_precision(self, confusion_mat):
+#         return numpy.diagonal(confusion_mat) / numpy.sum(confusion_mat, axis=0)
+
+#     def calculate_class_recall(self, confusion_mat):
+#         return numpy.diagonal(confusion_mat) / numpy.sum(confusion_mat, axis=1)
+
+#     def test(self):
+#         self.net.eval()
+
+#         correct = 0
+#         total = 0
+#         targets_ = []
+#         pred_ = []
+#         loss = 0.0
+#         with torch.no_grad():
+#             for (images, labels) in self.test_data_loader:
+#                 images, labels = images.to(self.device), labels.to(self.device)
+
+#                 outputs = self.net(images)
+#                 _, predicted = torch.max(outputs.data, 1)
+#                 total += labels.size(0)
+#                 correct += (predicted == labels).sum().item()
+
+#                 targets_.extend(labels.cpu().view_as(predicted).numpy())
+#                 pred_.extend(predicted.cpu().numpy())
+
+#                 loss += self.loss_function(outputs, labels).item()
+
+#         accuracy = 100 * correct / total
+#         confusion_mat = confusion_matrix(targets_, pred_)
+
+#         class_precision = self.calculate_class_precision(confusion_mat)
+#         class_recall = self.calculate_class_recall(confusion_mat)
+
+#         self.args.get_logger().debug('Test set: Accuracy: {}/{} ({:.0f}%)'.format(correct, total, accuracy))
+#         self.args.get_logger().debug('Test set: Loss: {}'.format(loss))
+#         self.args.get_logger().debug("Classification Report:\n" + classification_report(targets_, pred_))
+#         self.args.get_logger().debug("Confusion Matrix:\n" + str(confusion_mat))
+#         self.args.get_logger().debug("Class precision: {}".format(str(class_precision)))
+#         self.args.get_logger().debug("Class recall: {}".format(str(class_recall)))
+
+#         return accuracy, loss, class_precision, class_recall
+
+
+
+
+
+
+
+
+
+
 import torch
 import torch.optim as optim
 from sklearn.metrics import confusion_matrix
@@ -263,7 +446,7 @@ class Client:
         #     eps=self.args.get_eps()
         # )
 
-        # Use RMSprop optimizer
+        # # Use RMSprop optimizer
         # self.optimizer = optim.RMSprop(
         #     self.net.parameters(),
         #     lr=self.args.get_learning_rate(),
@@ -272,9 +455,13 @@ class Client:
         #     weight_decay=0,
         #     momentum=self.args.get_momentum()
         # )
-         self.optimizer = optim.SGD(self.net.parameters(),
-             lr=self.args.get_learning_rate(),
-             momentum=self.args.get_momentum())
+
+        # Use SGD optimizer
+        self.optimizer = optim.SGD(
+            self.net.parameters(),
+            lr=self.args.get_learning_rate(),
+            momentum=self.args.get_momentum()
+        )
 
         # Scheduler remains the same
         self.scheduler = MinCapableStepLR(
@@ -408,4 +595,5 @@ class Client:
         self.args.get_logger().debug("Class recall: {}".format(str(class_recall)))
 
         return accuracy, loss, class_precision, class_recall
+
 
