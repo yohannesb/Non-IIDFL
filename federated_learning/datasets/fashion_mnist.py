95ae75d418439d7c9ace809676166042145c966a
yohannesb <yohannesb@users.noreply.github.com>
2025-03-14 11:19:44 +0300

    Update fashion_mnist.py

--- federated_learning/datasets/fashion_mnist.py
+++ federated_learning/datasets/fashion_mnist.py
@@ -360,7 +360,7 @@ class FashionMNISTDataset(Dataset):
         self.get_args().get_logger().debug("Finished loading Fashion MNIST test data")
         return test_data
 
-    def flip_labels(self, dataset, flip_percentage=0.2, source_class=5, target_class=3):
+    def flip_labels(self, dataset, flip_percentage=1,0, source_class=5, target_class=3):
         targets = np.array(dataset.targets)
 
         # Find samples of the source class
